1fface5bcd11d4371f5d697ac70b116a1ac9b8a0
saulo <saulodavi2001@gmail.com>
2023-05-26 14:59:21 -0300

    pre deploy backend

--- src/src/middleware/schemaValidation.ts
+++ src/src/middleware/schemaValidation.ts
@@ -3,10 +3,8 @@ import { Request, Response, NextFunction } from "express";
 export function schemaValidation(schema: {validate: Function}) {
 
     return (req: Request, res: Response, next: NextFunction) => {
-        console.log('Req.body in schema: ', req.body)
         const { error } = schema.validate(req.body, { abortEarly: false });
         if (error) {
-            console.log('error detalhe :', error.details.map((detail: any) => detail.message))
             return res.status(400).send(error.details.map((detail: any) => detail.message));
         }
 
